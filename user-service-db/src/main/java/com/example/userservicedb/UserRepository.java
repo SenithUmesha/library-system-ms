8808b0c45b2c82136fa603eba0a63c7c3cad3aaf
Senith Umesha <90299964+SenithUmesha@users.noreply.github.com>
2026-09-17 21:45:11 +0530

    refactor: make user lookup password-agnostic

--- user-service-db/src/main/java/com/example/userservicedb/UserRepository.java
+++ user-service-db/src/main/java/com/example/userservicedb/UserRepository.java
@@ -1,31 +1,15 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.example.userservicedb;
 
-import java.util.List;
+import java.util.Optional;
 import javax.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-/**
- *
- * @author Senith Umesha
- */
-
 @Repository
-public interface UserRepository extends JpaRepository<User, Integer>{
- 
-         
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    Optional<User> findFirstByName(String name);
+
     @Transactional
-    @Modifying
-    @Query("delete from User u where u.name=?1")
-    void deleteUserByName(String name);
-    
-    @Query("select u from User u where u.name=?1 and u.password=?2")
-    List<User> findUserByNameAndPassword(String name, String password);
+    void deleteByName(String name);
 }
